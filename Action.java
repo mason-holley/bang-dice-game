bb74b6df946816ef5cbc9d3e3fff919f1d0adf6d
Vincent Hew <vncent5779@gmail.com>
2020-04-25 13:28:57 -0500

    new update

--- Action.java
+++ Action.java
@@ -1,27 +1,100 @@
 
 package bang_game;
-
 /**
  *
  * @author vincenthew
  */
 public class Action {
+    private Player actPlayer;
+    public Action(Player self) {
+        actPlayer = self;
+    }
+    
     public void actArrows() {
-        
+        int arrowHeld = 0;
+        for(int i = 0; i < TB.getList().size(); i++) {
+            arrowHeld = TB.getList().get(i).getArrow();
+            loseHP(TB.getList().get(i), arrowHeld);
+            TB.getList().get(i).resetSelfArrow();
+            addArrowBack(arrowHeld);
+        }
     }
     public void actDynamite() {
-        
+        loseHP(actPlayer, 1);
     }
     public void actGatling() {
-        
+        for(int i = 0; i < TB.getList().size(); i++) {
+            if(TB.getList().get(i).equals(actPlayer) == false) 
+                loseHP(TB.getList().get(i), 1);
+            addArrowBack(actPlayer.getArrow());
+            actPlayer.resetSelfArrow();
+        }
     }
-    public void actBullet_1() {
-        
+    public void actBullet_1(int direction) {
+        int target = 0;
+        if(direction == 0) {
+            target = leftTargetPos(TB.getList().indexOf(actPlayer));
+            loseHP(TB.getList().get(target), 1);
+        }
+        else if(direction == 1) {
+            target = rightTargetPos(TB.getList().indexOf(actPlayer));
+            loseHP(TB.getList().get(target), 1);
+        }
     }
-    public void actBullet_2() {
-        
+    public void actBullet_2(int direction) {
+        int target = 0;
+        if(TB.getList().size() == 3 || TB.getList().size() == 2)
+            actBullet_1(direction);
+        else if(TB.getList().size() > 3 && direction == 0) {
+            target = leftTargetPos((TB.getList().indexOf(actPlayer))+1);
+            loseHP(TB.getList().get(target), 1);
+        }
+        else if(TB.getList().size() > 3 && direction == 1) {
+            target = rightTargetPos((TB.getList().indexOf(actPlayer))-1);
+            loseHP(TB.getList().get(target), 1);
+        }
     }
-    public void actBeer() {
-        
+    public void actBeer(int target) {
+        gainHP(TB.getList().get(target), 1);
+    }
+    public void loseHP(Player actor, int hpVal) {
+        actor.setHP(actor.getHP() - hpVal);
+        if(actor.getHP() <= 0) {
+            //dead condition
+            actor.setHP(0);
+        }
+    }
+    public void gainHP(Player actor, int hpVal) {
+        actor.setHP(actor.getHP() + hpVal);
+        if(actor.getHP() >= 9) {
+            //over heal condition
+            actor.setHP(9);
+        }
+    }
+    public void gainArrow() {
+        actPlayer.addArrow(1);
+    }
+    public void addArrowBack(int val) {
+        Dice.setArrowPile(Dice.getArrowPile() + val);
+    }
+    
+    //targeting system
+    public static int leftTargetPos(int self) {
+        int targetPos;
+        targetPos = self + 1;
+        //when the self position is at the end of list
+        if(targetPos >= TB.getList().size()) {
+            targetPos = targetPos - TB.getList().size();
+        }
+        return targetPos;
+    }
+    public static int rightTargetPos(int self) {
+        int targetPos;
+        targetPos = self - 1;
+        //when the self position is at the begining of list
+        if(targetPos < 0) {
+            targetPos = targetPos + TB.getList().size();
+        }
+        return targetPos;
     }
 }
--- Dice.java
+++ Dice.java
@@ -15,51 +15,71 @@ import java.util.Random;
  */
 public class Dice{
     private int diceResult;
+    private boolean lock;
+    private static boolean globalLockDown;
+    private int solveCount;
     private static int arrows;
     private static int dynamite;
     private static int gatling;
-    private boolean lock;
+    private static int firstRoll;
     public static int rerollCount;
-    private Action diceAct = new Action();
+    private Action diceAct;
     
-    public Dice() {
+    public Dice(Player self) {
         diceResult = 0;
         arrows = 9;
         dynamite = 0;
         gatling = 0;
         lock = false;
+        globalLockDown = false;
         rerollCount = 0;
+        solveCount = 0;
+        firstRoll = 0;
+        diceAct = new Action(self);
     }
     
     public void rollDice(){
         Random randDice = new Random();
+        if(rerollCount == 0) {
+            firstRoll++;
+        }
         //dice cannot reroll more than twice
         if(rerollCount > 2) {
             lock = true;
+            globalLockDown = true;
         }
-        if(lock == false) {
+        if(lock == false && globalLockDown == false) {
             diceResult = randDice.nextInt(6)+1;
         }
-        else if(lock == true) {
-            System.out.println(">ALERT: Dice cannot be rerolled.");
+        else if(lock == true || globalLockDown == true) {
+            if(dynamite >= 3 && firstRoll == 5) {
+                System.out.println(">ALERT: Global lock down");
+            }
+            else
+                System.out.println(">ALERT: Dice cannot be rerolled.");
             diceResolve();
         }
         //resolve arrows and dynamite immediately
-        if(lock == false) {
+        if(lock == false && globalLockDown == false) {
             switch(diceResult) {
                 case 1:
                     arrows--;
+                    diceAct.gainArrow();
+                    //when the arrow pile is empty
                     if(arrows == 0) {
                         diceAct.actArrows();
-                        System.out.println(">Trigger: indian attack due to 0 arrows left");
                     }
                     break;
                 case 2:
                     dynamite++;
                     lock = true;
-                    if(dynamite >= 3) {
+                    //when there are three dyn
+                    if(dynamite >= 3 && firstRoll == 5) {
+                        globalLockDown = true;
                         diceAct.actDynamite();
-                        System.out.println(">Trigger: turn ends immediately due to " + dynamite + " dynamites");
+                        for(int i = 0; i < Bang_game.diceList.size(); i++) {
+                            Bang_game.diceList.get(i).rollDice();
+                        }
                     }
                     break;
             }
@@ -80,23 +100,39 @@ public class Dice{
     }
     
     private void diceResolve(){
-        switch(diceResult) {
-            case 3:
-                diceAct.actBullet_1();
-                break;
-            case 4:
-                diceAct.actBullet_2();
-                break;
-            case 5:
-                diceAct.actBeer();
-                break;
-            case 6:
-                gatling++;
-                if(gatling >= 3) {
-                    diceAct.actGatling();
-                }
-                break;                
+        Random randDecision = new Random();
+        int targetDecision = 0;
+        solveCount++;
+        if(solveCount == 1) {
+            switch(diceResult) {
+                case 3:
+                    targetDecision = randDecision.nextInt(1);
+                    diceAct.actBullet_1(targetDecision);
+                    break;
+                case 4:
+                    targetDecision = randDecision.nextInt(1);
+                    diceAct.actBullet_2(targetDecision);
+                    break;
+                case 5:
+                    targetDecision = randDecision.nextInt(TB.getList().size());
+                    diceAct.actBeer(targetDecision);
+                    break;
+                case 6:
+                    gatling++;
+                    if(gatling >= 3) {
+                        diceAct.actGatling();
+                    }
+                    break;                
+            }
         }
+    }
+    
+    public static int getArrowPile() {
+        int temp = arrows;
+        return temp;
+    }
+    public static void setArrowPile(int val) {
+        arrows = val;
     }
     
     public void resetDice() {
@@ -104,6 +140,9 @@ public class Dice{
         dynamite = 0;
         gatling = 0;
         rerollCount = 0;
+        solveCount = 0;
+        firstRoll = 0;
         lock = false;
+        globalLockDown = false;
     }
 }
